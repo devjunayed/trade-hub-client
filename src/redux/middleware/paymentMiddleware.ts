76f6b63241d3cb0632cd0f2234381907de6c61ef
devjunayed <devjunayed@gmail.com>
2025-02-02 22:56:47 +0600

    redeploy

--- src/redux/middleware/paymentMiddleware.ts
+++ src/redux/middleware/paymentMiddleware.ts
@@ -6,7 +6,7 @@ import { clearCart } from "@/redux/features/cart/cartSlice";
 const paymentMiddleware: Middleware = (store) => (next) => (action: any) => { 
  
     console.log(action.type);
-    if (action.type !== clearCart.type) {
+    if (typeof window !== "undefined" && action.type !== clearCart.type) {
         if (localStorage.getItem("clearCartAfterRedirect")) {
             localStorage.removeItem("clearCartAfterRedirect");
             localStorage.removeItem("persist:root")
