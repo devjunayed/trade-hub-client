04b0315ad4eba29b0ab8a4a8f72b8c1675979bb9
devjunayed <devjunayed@gmail.com>
2025-09-20 00:02:31 +0600

    light mode dark mode Theme switcer bug fix

--- src/components/Shared/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/Shared/ThemeSwitcher/ThemeSwitcher.tsx
@@ -8,20 +8,30 @@ import { MdLightMode } from "react-icons/md";
 import { MdOutlineLightMode } from "react-icons/md";
 
 export function ThemeSwitcher() {
-  const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
-  if (!mounted) return null
-
+  if (!mounted) return null;
 
   return (
     <div className="flex items-center">
-      <button className={`${theme === "light" && "hidden"}`} onClick={() => setTheme('light')}><MdLightMode size={18} /></button>
-      <button className={`${theme === "dark" && "hidden"}`} onClick={() => setTheme('dark')}><MdOutlineLightMode size={18} /></button>
+      {theme === "light" ? (
+        <button
+          onClick={() => setTheme("dark")}
+        >
+          <MdLightMode size={18} />
+        </button>
+      ) : (
+        <button
+          onClick={() => setTheme("light")}
+        >
+          <MdOutlineLightMode size={18} />
+        </button>
+      )}
     </div>
-  )
-};
+  );
+}
